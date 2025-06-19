e081150b9eea5e8eebc9c2bf838cf7cdf1eba42e
mohit-sonii <mohitsoni9731@gmail.com>
2025-06-19 15:11:06 +0530

    Frontend -> Modified Header to show the current logged in  instead of a select tag with all the other users where we can see any one to dos

--- Frontend/src/Header/Header.jsx
+++ Frontend/src/Header/Header.jsx
@@ -1,53 +1,58 @@
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-hot-toast";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { updateCurrentUser } from "../store/usernameSlice";
+import {  useSelector } from "react-redux";
+// import { toast } from "react-hot-toast";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { updateCurrentUser } from "../store/usernameSlice";
+// import { current } from "@reduxjs/toolkit";
 
 const Header = () => {
-   const currentUser =
+   const username =
       useSelector((state) => state.username.username) ||
       localStorage.getItem("username");
-   const [allUser, setAllUsers] = useState();
-   const [fieldSelect, setFieldSelect] = useState();
-   const dispatch = useDispatch();
+
+      const currentUser = username.charAt(0).toUpperCase() + username.slice(1).toLowerCase()
+   // const [allUser, setAllUsers] = useState();
+   // const [fieldSelect, setFieldSelect] = useState();
+   // const dispatch = useDispatch();
    
-   const data = async () => {
-      try {
-         const result = await axios.get(
-            "http://localhost:3000/api/user/get-users",
-            // 'https://daily-rounds-full-dev.vercel.app/api/user/get-users'
-            {
-               withCredentials: true,
-            }
-         );
+   // const data = async () => {
+   //    try {
+   //       const result = await axios.get(
+   //          "http://localhost:3000/api/user/get-users",
+   //          // 'https://daily-rounds-full-dev.vercel.app/api/user/get-users'
+   //          {
+   //             withCredentials: true,
+   //          }
+   //       );
 
-         if (result.status === 200) {
-            setAllUsers(result.data.data);
-         }
-      } catch (err) {
-         console.log(err);
-         if (err.response.data) {
-            toast.error(err.response.data.message);
-         }
-      }
-   };
+   //       if (result.status === 200) {
+   //          setAllUsers(result.data.data);
+   //       }
+   //    } catch (err) {
+   //       console.log(err);
+   //       if (err.response.data) {
+   //          toast.error(err.response.data.message);
+   //       }
+   //    }
+   // };
 
-   useEffect(() => {
-      data();
-   }, [currentUser]);
+   // useEffect(() => {
+   //    data();
+   // }, [currentUser,dispatch]);
 
-   const handleNameChange = (e) => {
-      setFieldSelect(e.target.value);
-      dispatch(updateCurrentUser(e.target.value));
-   };
+   // const handleNameChange = (e) => {
+   //    setFieldSelect(e.target.value);
+   //    dispatch(updateCurrentUser(e.target.value));
+   //    localStorage.setItem('username',e.target.value)
+   // };
 
    return (
       <div className="flex w-full px-6 py-7  bg-white shadow-md justify-between items-center">
          <h1 className="font-bold text-3xl text-gray-900">ToDo List</h1>
          <div className="flex flex-row gap-4">
             <button className="px-6 button py-2">Export</button>
-            <select
+            <p className="px-6 py-2 shadow-md font-semibold text-gray-800">Welcome,{"    "}{currentUser}</p>
+            {/* <select
                className="flex px-6 py-2 justify-center border-gray-500 border-1 bg-gray-100 cursor-pointer items-center rounded-md  outline-0 shadow-md"
                onChange={(e) => handleNameChange(e)}
             >
@@ -62,7 +67,7 @@ const Header = () => {
                         {item.username}
                      </option>
                   ))}
-            </select>
+            </select> */}
          </div>
       </div>
    );
